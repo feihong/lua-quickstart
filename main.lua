47f81d308e2b66a62965f5e5028e0a1e72ed0cd6
Feihong Hsu <feihong.hsu@gmail.com>
2023-02-03 10:23:45 -0600

    Allow rectangle to move according to keyboard presses

--- main.lua
+++ main.lua
@@ -1,18 +1,43 @@
 function love.load()
   x = 100
-  delta = 100
+  y = 50
+  xDelta = 0
+  yDelta = 0
+  width = 200
+  height = 150
+  step = 200
+
+  winWidth, winHeight = love.graphics.getDimensions()
 end
 
 function love.draw()
-  love.graphics.rectangle("line", x, 50, 200, 150)
+  love.graphics.rectangle("line", x, y, width, height)
+end
+
+function getWithinBounds(v, lower, upper)
+  if v < lower then 
+    return lower
+  elseif v > upper then
+    return upper
+  else
+    return v
+  end
 end
 
 function love.update(dt)
-  x = x + delta * dt
+  x = getWithinBounds(x + xDelta * dt, 0, winWidth - width)
+  y = getWithinBounds(y + yDelta * dt, 0, winHeight - height)
 
-  if x <= 0 then 
-    delta = 100
-  elseif x >= 600 then
-    delta = -100
+  if love.keyboard.isDown('right') then
+    xDelta = step
+  elseif love.keyboard.isDown('left') then
+    xDelta = -1 * step
+  elseif love.keyboard.isDown('up') then
+    yDelta = -1 * step
+  elseif love.keyboard.isDown('down') then
+    yDelta = step
+  else
+    xDelta = 0
+    yDelta = 0
   end
 end
