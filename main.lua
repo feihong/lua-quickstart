5fba322f94acf666f8bc6846dc6168e21df71cd6
Feihong Hsu <feihong.hsu@gmail.com>
2023-02-02 23:09:19 -0600

    Draw moving rectangle

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+function love.load()
+  x = 100
+  delta = 100
+end
+
 function love.draw()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.rectangle("line", x, 50, 200, 150)
+end
+
+function love.update(dt)
+  x = x + delta * dt
+
+  if x <= 0 then 
+    delta = 100
+  elseif x >= 600 then
+    delta = -100
+  end
 end
