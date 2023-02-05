41f19a2c9248e62bb8353b32e37f6f687a7be6de
Feihong Hsu <feihong.hsu@gmail.com>
2023-02-05 09:09:36 -0600

    Put helper functions inside util.lua

--- main.lua
+++ main.lua
@@ -1,36 +1,11 @@
-function polarToCartesian(r, theta)
-  theta = math.rad(theta)
-  return {x = r * math.cos(theta), y = r * math.sin(theta)}
-end
-
-function changeRectDeltas(angle)
-  for _, rect in ipairs(rects) do
-    rect.delta = polarToCartesian(rect.speed, angle)
-  end
-end
-
-function makeRectangle()
-  local width = math.random(100, 200)
-  local height = math.random(100, 200)
-  local speed = math.random(100, 400)
-  local delta = polarToCartesian(speed, math.random(0, 360))
-
-  return { 
-    x = math.random(0, winWidth - width), 
-    y = math.random(0, winHeight - height), 
-    speed = speed,
-    delta = delta,
-    width = width,
-    height = height,
-  }
-end
+require('util')
 
 function love.load()
   math.randomseed(os.time())
 
   winWidth, winHeight = love.graphics.getDimensions()
 
-  rects = {makeRectangle()}
+  rects = {makeRectangle(winWidth, winHeight)}
 
   fruits = {'apples'}
   extraFruits = {'bananas', 'cherry', 'durian', 'elderberry', 'fig', 'grape', 'honeydew'}
@@ -77,7 +52,7 @@ function love.keypressed(key)
       rect.delta = polarToCartesian(rect.speed, math.random(0, 360))
     end
   elseif key == 'space' then
-    table.insert(rects, makeRectangle())
+    table.insert(rects, makeRectangle(winWidth, winHeight))
   elseif key == 'right' then
     changeRectDeltas(0)
   elseif key == 'left' then
